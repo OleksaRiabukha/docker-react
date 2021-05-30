112f0718e4b6cbd11b921e106ef763c797b372f8
A_Wheatman <oleksa.wheat@gmail.com>
2021-05-30 12:32:04 +0300

    fix tests

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          More ggggggg
+          Learn React
         </a>
       </header>
     </div>
